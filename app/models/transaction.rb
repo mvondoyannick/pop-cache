259b88029676ac1eb681edf5c1a5aee831197d9a
MVONDO Yannick <mvondoyannick@gmail.com>
2019-05-02 20:39:45 +0200

    update encaussement

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -5,7 +5,7 @@ class Transaction < ApplicationRecord
   private
   def set_color
     self.color = "#e53935" if self.flag == "PAIEMENT"     #rouge
-    self.color = "#283593" if self.flag == "ENCAISSEMENT" #violet
+    self.color = "#ffc107" if self.flag == "ENCAISSEMENT" #violet
     self.color = "#bf360c" if self.flag == "RETRAIT"      #Orange sombre, degré 900
     self.color = "#000000" if self.flag == "RECHARGE"     #noir
   end
