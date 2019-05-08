a9435ddcb2573f4b176305410d2e13a7c209c213
MVONDO Yannick <mvondoyannick@gmail.com>
2019-05-08 20:47:14 +0200

    update seed-2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Client::create_user(Faker::Name.name, Faker::FunnyName.two_word_name, '699599993', 2514635874, 123456, 'Masulin') 
-Client::create_user(Faker::Name.name, Faker::FunnyName.two_word_name, '699990994', 2514665873, 123456, 'Feminin' )
+#Client::create_user(Faker::Name.name, Faker::FunnyName.two_word_name, '699599993', 2514635874, 123456, 'Masulin')
+#Client::create_user(Faker::Name.name, Faker::FunnyName.two_word_name, '699990994', 2514665873, 123456, 'Feminin' )
 
 #gestion de 10000 transaction
 1000.times do |t|
