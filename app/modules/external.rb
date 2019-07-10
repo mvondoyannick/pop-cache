431b486700fa00553912675ee651af9ff069eb38
MVONDO Yannick <mvondoyannick@gmail.com>
2019-07-10 17:45:49 +0100

    adding user restriction account limit payment

--- app/modules/external.rb
+++ app/modules/external.rb
@@ -25,96 +25,47 @@ module External
 
       Rails::logger.info "Starting phone payement ..."
 
+      Rails::logger.info "Verifiying Account customer amount ..."
+
       # Recherche le customer pour authentification
       customer = Customer.find_by_authentication_token(@customer_token)
       if customer && customer.valid_password?(@customer_password)
 
-        Rails::logger.info "Customer found and data are ok ..."
+        Rails::logger.info "Customer/Payer found and all his data are ok ..."
 
-        #searching for merchant
-        merchant = Customer.find_by_phone(@merchant_phone)
-        if merchant.blank?
-          Rails::logger.info "Merchant phone don't found ..."
-          # on n'a pas trouver le marchand, on le cree directement et on credit son compte puis le le notifie par SMS
-          new_merchant = Customer.new(
-            email: Faker::Internet.email,
-            password: Faker::Internet.password(8),
-            name: Faker::Name.first_name,
-            second_name: Faker::Name.last_name,
-            phone: @merchant_phone,
-            cni: Faker::Code.imei,
-            type_id: 2,
-            sexe: Faker::Gender.binary_type
-          )
+        if customer.account.amount.to_f < @amount.to_f
 
-          #trying to save new demo customer
-          if new_merchant.save
-            Rails::logger.info "Save new merchant informations DB."
-            #trying to create new_merchant account and store data amount
-            new_merchant_account = Account.new(
-              customer_id: new_merchant.id,
-              amount: @amount
-            )
-
-            # creating account
-            if new_merchant_account.save
-
-              client = History.new(
-                customer_id: customer.id,
-                amount: Parametre::Parametre::agis_percentage(@amount),
-                context: 'none',
-                flag: 'paiement'.upcase,
-                code: "EXT_PAY_#{@hash}"
-              )
-
-              # Historique du client
-              if client.save
-
-                # Historique du marchand virtuel pour le moment
-                marchand = History.new(
-                  customer_id: customer.id,
-                  amount: @amount,
-                  context: 'none',
-                  flag: 'paiement'.upcase,
-                  code: "EXT_PAY_#{@hash}"
-                )
-
-                if marchand.save
-
-                  Rails::logger.info "Save new merchant account information"
-                  Sms.sender(@merchant_phone, "Bonjour, un Paiement de #{@amount} F CFA vient d etre effectue dans votre compte virtuel/numero de telephone #{@merchant_phone}. ID de la transaction EXT_PAY_#{@hash}")
-                  return true, "Paiement effectué d'un montant de #{Parametre::Parametre::agis_percentage(@amount)} à #{@merchant_phone}"
-
-                else
-
-                  return false, "Impossible de mettre a jour l'historique du marchand virtuel : #{marchand.errors.full_messages}"
-
-                end
-              else
-
-                return false, "Impossible de mettre a jour l'historique du payeur/Client : #{client.errors.full_messages}"
-
-              end
-            end
-          end
+          return false, "Votre compte est insuffisant pour effectuer la transaction. Merci de le recharger"
 
         else
-          #merchant exist in DB, juste update his account
-          Rails::logger.info "The merchant exist in the DB ..."
-          if customer.account.update(amount: customer.account.amount.to_f - @amount.to_f)
 
-            m_account = Customer.find_by_phone(@merchant_phone).account
+          #searching for merchant
+          merchant = Customer.find_by_phone(@merchant_phone)
+          if merchant.blank?
+            Rails::logger.info "Merchant phone don't found inside the plateforme..."
+            # on n'a pas trouver le marchand, on le cree directement et on credit son compte puis le le notifie par SMS
+            new_merchant = Customer.new(
+              email: Faker::Internet.email,
+              password: Faker::Internet.password(8),
+              name: Faker::Name.first_name,
+              second_name: Faker::Name.last_name,
+              phone: @merchant_phone,
+              cni: Faker::Code.imei,
+              type_id: 2,
+              sexe: Faker::Gender.binary_type
+            )
 
-            if m_account.update(amount: m_account.amount.to_f + @amount.to_f)
-              marchant = History.new(
-                customer_id: Customer.find_by_phone(@merchant_phone).id,
-                amount: @amount,
-                context: 'none',
-                flag: 'encaissement'.upcase,
-                code: "EXT_PAY_#{@hash}"
+            #trying to save new demo customer
+            if new_merchant.save
+              Rails::logger.info "Save new merchant informations DB."
+              #trying to create new_merchant account and store data amount
+              new_merchant_account = Account.new(
+                customer_id: new_merchant.id,
+                amount: @amount
               )
 
-              if marchant.save
+              # creating account
+              if new_merchant_account.save
 
                 client = History.new(
                   customer_id: customer.id,
@@ -124,29 +75,97 @@ module External
                   code: "EXT_PAY_#{@hash}"
                 )
 
+                # Historique du client
                 if client.save
-                  Sms.sender(@merchant_phone, "Bonjour, un Paiement d'un montant de #{@amount} F CFA vient d etre effectue dans votre compte #{@merchant_phone}. ID transaction EXT_PAY_#{@hash}. Le solde de votre compte est maintenant de #{m_account.amount} F CFA")
-                  return true, "Paiement d'un montant de #{Parametre::Parametre::agis_percentage(@amount)} effectué à #{@merchant_phone}."
+
+                  # Historique du marchand virtuel pour le moment
+                  marchand = History.new(
+                    customer_id: customer.id,
+                    amount: @amount,
+                    context: 'none',
+                    flag: 'paiement'.upcase,
+                    code: "EXT_PAY_#{@hash}"
+                  )
+
+                  if marchand.save
+
+                    Rails::logger.info "Save new merchant account information"
+                    Sms.sender(@merchant_phone, "Bonjour, un Paiement de #{@amount} F CFA vient d etre effectue dans votre compte virtuel/numero de telephone #{@merchant_phone}. ID de la transaction EXT_PAY_#{@hash}. Rapprochez-vous d'une agence UBA ou creer un compte PayMeQuick.")
+                    return true, "Paiement effectué d'un montant de #{Parametre::Parametre::agis_percentage(@amount)} à #{@merchant_phone}"
+
+                  else
+
+                    return false, "Impossible de mettre a jour l'historique du marchand virtuel : #{marchand.errors.full_messages}"
+
+                  end
                 else
-                  return false, "Impossible de sauver l'historique : #{client.errors.full_messages}"
+
+                  return false, "Impossible de mettre a jour l'historique du payeur/Client : #{client.errors.full_messages}"
+
                 end
-
-              else
-                return false, "Impossible de sauver l'historique : #{marchand.errors.full_messages}"
               end
-
-            else
-              return false, "Une erreur est survenue durant la transaction de paiement : #{m_account.errors.full_messages}"
             end
 
           else
+            #merchant exist in DB, juste update his account
+            Rails::logger.info "The merchant exist in the DB ..."
+            if customer.account.update(amount: customer.account.amount.to_f - @amount.to_f)
 
-            return false, "Impossible de mettre à jour les informations du client"
+              m_account = Customer.find_by_phone(@merchant_phone).account
+
+              if m_account.update(amount: m_account.amount.to_f + @amount.to_f)
+                marchant = History.new(
+                  customer_id: Customer.find_by_phone(@merchant_phone).id,
+                  amount: @amount,
+                  context: 'none',
+                  flag: 'encaissement'.upcase,
+                  code: "EXT_PAY_#{@hash}"
+                )
+
+                if marchant.save
+
+                  client = History.new(
+                    customer_id: customer.id,
+                    amount: Parametre::Parametre::agis_percentage(@amount),
+                    context: 'none',
+                    flag: 'paiement'.upcase,
+                    code: "EXT_PAY_#{@hash}"
+                  )
+
+                  if client.save
+                    Sms.sender(@merchant_phone, "Bonjour, un Paiement d'un montant de #{@amount} F CFA vient d etre effectue dans votre compte #{@merchant_phone}. ID transaction EXT_PAY_#{@hash}. Le solde de votre compte est maintenant de #{m_account.amount} F CFA. Rapprochez-vous d'une agence UBA ou creer un compte PayMeQuick.")
+                    return true, "Paiement d'un montant de #{Parametre::Parametre::agis_percentage(@amount)} effectué à #{@merchant_phone}."
+                  else
+                    return false, "Impossible de sauver l'historique : #{client.errors.full_messages}"
+                  end
+
+                else
+
+                  Rails::logger::info "Failed to save history on the plateforme"
+                  return false, "Impossible de sauver l'historique : #{marchand.errors.full_messages}"
+                  
+                end
+
+              else
+
+                Rails::logger::info "Transaction failed during process"
+                return false, "Une erreur est survenue durant la transaction de paiement : #{m_account.errors.full_messages}"
+
+              end
+
+            else
+
+              Rails::logger::info "Impossible de mettre a jour les informations du client"
+              return false, "Impossible de mettre à jour les informations du client"
+
+            end
 
           end
 
         end
+
       else
+        Rails::logger::info "User password invalid"
         return false, "Mot de passe invalid"
       end
 
