88542bb37dc3e9e40e33bb96e6a22d8050df6c25
MVONDO Yannick <mvondoyannick@gmail.com>
2019-11-29 16:07:56 +0100

    update jobs & client #5

--- app/jobs/pay_job.rb
+++ app/jobs/pay_job.rb
@@ -9,7 +9,7 @@ class PayJob < ApplicationJob
   # @params [Float] solde
   # @params [String] hash
   # @params [Integer] id
-  def perform(amount, phone, type, complete_name=nil, solde=nil, hash, frais=nil, total=nil, id)
+  def perform(amount, phone, type, complete_name=nil, solde=nil, hash, id)
     @phone = phone
     @amount = amount
     @type = type
@@ -17,15 +17,13 @@ class PayJob < ApplicationJob
     @solde = solde
     @hash = hash
     @id = id
-    @frais = frais
-    @total_preleve = total
 
     #sending SMS like à job after 2 seconds
     case type
     when "merchant"
       Sms.nexah(@phone, "Paiement d'un montant de #{@amount} FCFA recu de Mr/Mme #{@complete_name}. Votre Solde est actuellement de :  #{@solde} F CFA. ID Transaction : #{@hash}. Plus sur d'informations sur https://payquick-develop.herokuapp.com/webview/#{@hash}/#{@id}")
     when "payer"
-      Sms.nexah(@phone, "Paiement de #{@amount} F CFA dont les frais s'élève à #{@frais} FCFA. Le montant total prélevé de votre compte est de #{total_preleve} FCFA. Plus sur https://payquick-develop.herokuapp.com/webview/#{@hash}/#{@id}")
+      Sms.nexah(@phone, "Paiement de #{@amount} F CFA, Paiement effectué avec succès. Plus sur https://payquick-develop.herokuapp.com/webview/#{@hash}/#{@id}")
     else
       # sending errors to admins
       return false, "Type de paiement inconnu"
