d827fa19f5ff79771d1e7aea107b6249a7401b65
MVONDO Yannick <mvondoyannick@gmail.com>
2019-12-01 22:28:32 +0100

    adding https to leaflet

--- config/initializers/leaflet.rb
+++ config/initializers/leaflet.rb
@@ -1,3 +1,3 @@
-Leaflet.tile_layer = "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+Leaflet.tile_layer = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 Leaflet.attribution = "PAYMEQUICK MAPS #{Date.today.year}"
 Leaflet.max_zoom = 18
